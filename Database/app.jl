8a658a6c00a6c6c5a0bc6ef81ff0c526b464d2e7
PGimenez <gimenezfebrer@gmail.com>
2022-12-20 21:38:07 +0100

    ui

--- Database/app.jl
+++ Database/app.jl
@@ -1,7 +1,36 @@
-using RDatasets, DataFrames, Flux, Random
-using MLDatasets: BostonHousing
-using BSON: @save, @load
+using DataFrames, Flux
+using BSON: @load
+using SearchLight, SearchLightSQLite
+using GenieFramework
+include("app/resources/houses/Houses.jl")
+using .Houses
+@genietools 
 
-@load "bostonflux.bson" model x_test y_test
+SearchLight.Configuration.load() |> SearchLight.connect
+data = DataFrame(all(House))
 
-@show model(x_test)
+@load "bostonflux.bson" model
+X_features = Matrix(data[:,2:end-1])
+y = Vector(data[:, end])
+N = length(y)
+
+@handlers begin
+    @in idx = 0  
+    @out x = []
+    @out y = 0
+    @onchange idx begin
+        x = X_features[Int(idx%N+1),:]
+        y = model(x) 
+    end
+end
+
+function ui()
+    [
+        h5("Predict house price")
+        row(btn("Predict!", color="red", icon="mail", type="a", @click("idx=idx+1")))
+        bignumber(label="Predicted price", value=:y)
+    ]
+end
+
+@page("/", ui)
+Server.isrunning() || Server.up()
