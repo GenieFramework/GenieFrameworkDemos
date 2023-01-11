e2473e1e5511452929c86f90e0303142bdd29689
PGimenez <gimenezfebrer@gmail.com>
2023-01-11 16:34:54 +0100

    change id var name

--- Database/app.jl
+++ Database/app.jl
@@ -11,20 +11,20 @@ using .Houses, .Predictions
 Configuration.load() |> connect
 @load "bostonflux.bson" model
 
-@show findone(House, id=1)
 @handlers begin
     @out datatable = DataFrame(all(House))[:,2:end] |> x -> insertcols(x, 1, :id => 1:count(House)) |> DataTable
     @out predtable = DataTable()
     @out datatablepagination = DataTablePagination(rows_per_page=50)
-    @in id = 0 
+    @in hid = 0 
     @out house = House()
     @out prediction = Prediction()
 
-    @onchange id begin
-        house = findone(House, id=1)
-        prediction = Prediction()
+    @onchange hid begin
+        house = findone(House, id=hid)
+        # prediction = Prediction()
         prediction.house_id = house.id
         prediction.price = model([getfield(house, i) for i in 2:14])[1]
+        @show prediction.price
         prediction.error = abs(prediction.price - house.MEDV)
         prediction.timestamp = string(now())
         save!(prediction)
@@ -34,14 +34,15 @@ end
 
 function ui()
 [
-        h2(class="q-mx-auto", "Boston house prices")
+        h2(class="q-mx-auto", style="margin:auto;display:block", "Boston house prices")
         row(
         [
                 cell(class="st-module card", style="width:400px", [
                     h6("Predict house price")
-                    # textfield("House id", :id)
+                    textfield("House id", :hid)
                     bignumber("Real price", R"house.MEDV")
                     bignumber("Predicted price", R"prediction.price")
+                    cell(class="", style="margin:auto;display:block")
                 ])
                 cell(class="st-module", [
                     h6("Prediction history")
