04fea383486c60d7f7641388fd4b81a95459a0f2
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-01-26 18:59:31 +0530

    CSV upload fixed Base.notify

--- CSVAnalysis/dashboard.jl
+++ CSVAnalysis/dashboard.jl
@@ -1,6 +1,7 @@
 using GenieFramework, DataFrames, CSV
 @genietools
 
+# @uploadheaders
 Genie.config.cors_headers["Access-Control-Allow-Origin"]  =  "*"
 Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
 Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
@@ -9,37 +10,73 @@ Genie.config.cors_allowed_origins = ["*"]
 const FILE_PATH = "upload"
 mkpath(FILE_PATH)
 
-@out title = "CSV Analysis"
-@out upfiles = readdir(FILE_PATH)
-@in selected_file = "iris.csv"
-@in selected_column = "petal.length"
-@out columns = ["petal.length", "petal.width", "sepal.length", "sepal.width", "variety"]
-@out irisplot = PlotData()
+@vars begin
+    title = "Limited Bandwidth Demo"
+    selected_file = "iris.csv"
+    selected_column = "petal.length"
+    upfiles = readdir(FILE_PATH)
+    columns = ["petal.length", "petal.width", "sepal.length", "sepal.width", "variety"]
+    irisplot = PlotData()
+    files_dirty = false
+end
 
-@handlers begin
-    @onchangeany isready, selected_file, selected_column begin
-        upfiles = readdir(FILE_PATH)
-        data = CSV.read(joinpath(FILE_PATH, selected_file), DataFrame)
-        columns = names(data)
-        datatable = DataTable(data)
-        if selected_column in names(data)
-            irisplot = PlotData(x=data[!, selected_column], plot=StipplePlotly.Charts.PLOT_TYPE_HISTOGRAM)
-        end
+@onchange isready, selected_file, selected_column, upfiles begin
+    filepath = joinpath(FILE_PATH, selected_file)
+    isfile(filepath) || return
+    data = CSV.read(filepath, DataFrame)
+    columns = names(data)
+    if selected_column in names(data)
+        irisplot = PlotData(x=data[!, selected_column], plot=StipplePlotly.Charts.PLOT_TYPE_HISTOGRAM)
     end
 end
 
-route("/", method = POST) do
-  files = Genie.Requests.filespayload()
-  for f in files
-      write(joinpath(FILE_PATH, f[2].name), f[2].data)
-  end
-  if length(files) == 0
-      @info "No file uploaded"
-  end
-  upfiles = readdir(FILE_PATH)
-  return "Upload finished"
+@event updatefiles begin
+    upfiles = readdir(FILE_PATH)
 end
+
+# function ui()
+#     [
+#         row([
+#             cell(class="col-md-12", [
+#                 uploader(label="Upload Dataset", accept=".csv", multiple=true, method="POST", url="http://localhost:8000/", field__name="csv_file",
+#                     var"@uploaded"="handle_event('', 'updatefiles')")
+#             ])
+#         ]),
+#         row([
+#             cell(class="st-module", [
+#                 h6("File")
+#                 Stipple.select(:selected_file; options=:upfiles)
+#             ])
+#             cell(class="st-module", [
+#                 h6("Column")
+#                 Stipple.select(:selected_column; options=:columns)
+#             ])
+#         ]),
+#         row([
+#             cell(class="st-module", [
+#                 h5("Histogram")
+#                 plot(:irisplot)
+#             ])
+#         ])
+#     ]
+# end
 
 
 @page("/", "ui.jl")
+
+# @uploadhandler("/", "dirpath")
+
+route("/", method = POST) do
+    @show "Processing upload..."
+    files = Genie.Requests.filespayload()
+    for f in files
+        write(joinpath(FILE_PATH, f[2].name), f[2].data)
+    end
+    if length(files) == 0
+        @info "No file uploaded"
+    end
+
+    return "Upload finished"
+end
+
 Server.isrunning() || Server.up()
