8c7237cbfd26c3dc021d48ff686bab3a79ae1fce
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-01-26 19:53:19 +0530

    bad api for upload using macros

--- CSVAnalysis/dashboard.jl
+++ CSVAnalysis/dashboard.jl
@@ -1,11 +1,7 @@
 using GenieFramework, DataFrames, CSV
 @genietools
 
-# @uploadheaders
-Genie.config.cors_headers["Access-Control-Allow-Origin"]  =  "*"
-Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
-Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
-Genie.config.cors_allowed_origins = ["*"]
+@uploadheaders
 
 const FILE_PATH = "upload"
 mkpath(FILE_PATH)
@@ -34,49 +30,7 @@ end
     upfiles = readdir(FILE_PATH)
 end
 
-# function ui()
-#     [
-#         row([
-#             cell(class="col-md-12", [
-#                 uploader(label="Upload Dataset", accept=".csv", multiple=true, method="POST", url="http://localhost:8000/", field__name="csv_file",
-#                     var"@uploaded"="handle_event('', 'updatefiles')")
-#             ])
-#         ]),
-#         row([
-#             cell(class="st-module", [
-#                 h6("File")
-#                 Stipple.select(:selected_file; options=:upfiles)
-#             ])
-#             cell(class="st-module", [
-#                 h6("Column")
-#                 Stipple.select(:selected_column; options=:columns)
-#             ])
-#         ]),
-#         row([
-#             cell(class="st-module", [
-#                 h5("Histogram")
-#                 plot(:irisplot)
-#             ])
-#         ])
-#     ]
-# end
-
-
 @page("/", "ui.jl")
-
-# @uploadhandler("/", "dirpath")
-
-route("/", method = POST) do
-    @show "Processing upload..."
-    files = Genie.Requests.filespayload()
-    for f in files
-        write(joinpath(FILE_PATH, f[2].name), f[2].data)
-    end
-    if length(files) == 0
-        @info "No file uploaded"
-    end
-
-    return "Upload finished"
-end
+@uploadhandler("/", "upload")
 
 Server.isrunning() || Server.up()
