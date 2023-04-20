08999e0b813bfac629a01a63ea971401cdd20716
Pere Giménez <gimenezfebrer@gmail.com>
2023-04-20 13:13:29 +0200

    map shows optimization result

--- TravellingSalesman/app.jl
+++ TravellingSalesman/app.jl
@@ -1,22 +1,83 @@
 module App
 include("tsp.jl")
 using .TSP
-using GenieFramework
+using GenieFramework, PlotlyBase, Random
 @genietools
 
-N = 9
+function haversine_distance(lat1, lon1, lat2, lon2)
+    R = 6371  # Earth's radius in kilometers
+    dLat = (lat2 - lat1) * π / 180
+    dLon = (lon2 - lon1) * π / 180
+    a = sin(dLat / 2)^2 + cos(lat1 * π / 180) * cos(lat2 * π / 180) * sin(dLon / 2)^2
+    c = 2 * atan(sqrt(a), sqrt(1 - a))
+    return R * c
+end
+
+function distance_matrix(cities)
+    n = length(cities)
+    @show typeof(cities)
+    dist_matrix = Matrix{Float64}(undef, n, n)
+    for i in 1:n
+        for j in 1:n
+            if i == j
+                dist_matrix[i, j] = 0
+            else
+                lat1, lon1 = cities[i]
+                lat2, lon2 = cities[j]
+                dist_matrix[i, j] = haversine_distance(lat1, lon1, lat2, lon2)
+            end
+        end
+    end
+    return dist_matrix
+end
 
 @app begin
-    @in D = [0.0 256.0 137.0 152.0 193.0 461.0 542.0 627.0 889.0; 256.0 0.0 125.0 209.0 422.0 206.0 313.0 848.0 719.0; 137.0 125.0 0.0 143.0 297.0 326.0 424.0 723.0 829.0; 152.0 209.0 143.0 0.0 337.0 414.0 484.0 776.0 737.0; 193.0 422.0 297.0 337.0 0.0 623.0 720.0 517.0 1075.0; 461.0 206.0 326.0 414.0 623.0 0.0 249.0 840.0 700.0; 542.0 313.0 424.0 484.0 720.0 249.0 0.0 1089.0 511.0; 627.0 848.0 723.0 776.0 517.0 840.0 1089.0 0.0 1552.0; 889.0 719.0 829.0 737.0 1075.0 700.0 511.0 1552.0 0.0]
-    @out X = zeros(9, 9)
+    @in cities = [
+        (51.5074, -0.1278),   # London
+        (40.7128, -74.0060),  # New York
+        (35.6895, 139.6917),  # Tokyo
+        (-33.8688, 151.2093), # Sydney
+        (37.7749, -122.4194), # San Francisco
+        (19.4326, -99.1332)   # Mexico City
+    ]
+    @out D = zeros(6, 6)
+    @out X = zeros(6, 6)
+    @out mapData = [scattergeo()]
+    @out mapLayout = PlotlyBase.Layout()
+    @out mapConfig = PlotlyBase.PlotConfig()
     @onchange isready begin
-        X = solve_tsp(D)
+        D = distance_matrix(cities)
+        X = TSP.solve_tsp(D)
+        connected_cities = [(cities[i], cities[j]) for i in 1:size(X, 1), j in 1:size(X, 2) if X[i, j] == 1.0]
+        @show connected_cities
+        # Create a trace for the cities with markers
+        trace_cities = scattergeo(
+            locationmode="ISO-3",
+            lat=[city[1] for city in cities],
+            lon=[city[2] for city in cities],
+            mode="markers",
+            marker=attr(size=10, color="blue")
+        )
+        # Create a trace for the line between the two selected cities
+        trace_line = scattergeo(
+            locationmode="ISO-3",
+            lat=reduce(vcat, [city[1] for city_pair in connected_cities for city in city_pair]),
+            lon=reduce(vcat, [city[2] for city_pair in connected_cities for city in city_pair]),
+            mode="lines",
+            line=attr(width=2, color="red")
+        )
+        mapData = [trace_cities, trace_line]
         @show X
     end
 end
 
-ui() = p("{{X}}")
+function ui()
+    [
+    h1("Travelling Salesman Problem")
+    plot(:mapData, layout=:mapLayout, config=:mapConfig)
+    ]
+end
 
 @page("/", ui)
-Server.up()
+Server.up(9000)
 end
